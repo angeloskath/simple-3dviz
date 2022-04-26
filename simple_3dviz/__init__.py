0221ce57ca7c7fdc0b34420b7887a49cbaf09764
Angelos Katharopoulos <katharas@gmail.com>
2022-04-25 23:18:27 -0700

    Bump the version and change the maintainer email

--- simple_3dviz/__init__.py
+++ simple_3dviz/__init__.py
@@ -1,13 +1,13 @@
 """Simple visualization of meshes using opengl with moderngl."""
 
 __maintainer__ = "Angelos Katharopoulos, Despoina Paschalidou"
-__email__ = "angelos.katharopoulos@idiap.ch"
+__email__ = "katharas@gmail.com"
 __url__ = "http://simple-3dviz.com"
 __description__ = "simple-3dviz is a simple visualization library for 3D "
 "data using Python and OpenGL."
 __keywords__ = "graphics geometry 3D"
 __license__ = "MIT"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from .renderables import Mesh, TexturedMesh, Spherecloud, Lines
 from .scenes import Scene
